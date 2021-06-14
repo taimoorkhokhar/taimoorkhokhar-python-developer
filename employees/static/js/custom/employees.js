92aec153e3087f73f9ec80ba97cecd4af88dc882
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-06-14 19:16:25 +0500

    add employees through api call

--- employees/static/js/custom/employees.js
+++ employees/static/js/custom/employees.js
@@ -1,87 +1,75 @@
 $(document).ready(function () {
-    // setTimeout(function(){
-        const myInput = document.querySelector("#employeeDatePicker");
-        // const config = {
-        //     altInput: true,
-        //     dateFormat: "YYYY-MM-DD",
-        //     altFormat: "DD-MM-YYYY",
-        //     allowInput: true,
-        //     wrap: true,
-        //     parseDate: (datestr, format) => {
-        //       return moment(datestr, format, true).toDate();
-        //     },
-        //     formatDate: (date, format, locale) => {
-        //       // locale can also be used
-        //       return moment(date).format(format);
-        //     }
-        // }
-        const fp = flatpickr(myInput, {});  // flatpickr
-        fp.config.onChange.push(function(selectedDates, dateStr, instance) {
-            console.log(dateStr)
-            getEmployeesByDate(datestr)
-        });
-        // var e = document.getElementById("selectEmployeeDropdown");
-        // var strUser = e.value;
-        // console.log("strUser", strUser)
+    let employeesDic = {};
+    const employeeDatePicker = document.querySelector("#employeeDatePicker");
+    let bookMeetingBtns = [];
 
-        // const chatBubbles = document.querySelectorAll('#mCSB_1 ul li a');
-        // const sendMessage = document.querySelector('#send-message');
-        // const chatProfileImage = $('.chat-profile-photo');
-        // const chatProfileName = $('.chat-profile-name');
-        // const chatOrganizationName = $('.chat-profile-name p');
-        // const chatChannelName = $('.chat-profile-name p');
-        // const typingAnimation = $('.chat-box .clearfix.typing-animation');
-        
+    const fp = flatpickr(employeeDatePicker, {});  // flatpickr
 
-        // for (let clickEventCounter = 0; clickEventCounter < chatBubbles.length; clickEventCounter++) {
-        //     chatBubbles[clickEventCounter].addEventListener('click',  chatBubblesClickEvent.bind(this, clickEventCounter), false);
-        // }
-        // sendMessage.addEventListener('click',  sendMessageClickEvent.bind(this, 0), false);
+    fp.config.onChange.push(function(selectedDates, dateStr, instance) {
+        console.log(dateStr)
+        getEmployeesByDate(dateStr)
+    });
 
 
-        // function sendMessageClickEvent(elementCounter, element){
-            
-        // }
+    function getEmployeesByDate(date) {
+        const form_data = new FormData();
+        form_data.append("date", date);
+        form_data.append("csrfmiddlewaretoken" , "{{csrf_token}}");
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/employees/2021-06-14/?format=json",
+            type:'GET',
+            data: form_data,
+            cache: false,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                employeesDic = response.response;
+                let counter = 1;
+                for (const dataDic of response.response) {
+                    console.log(dataDic)
 
-        function getEmployeesByDate(date) {
-            // console.log(collectiveName,channelName,assistantName, message)
-            const form_data = new FormData();
-            form_data.append("date", date);
-            form_data.append("csrfmiddlewaretoken" , "{{csrf_token}}");
-            $.ajax({
-                url: "http://127.0.0.1/api/ask-question/?format=json",
-                type:'POST',
-                data: form_data,
-                cache: false,
-                processData: false,
-                contentType: false,
-                success: function (response) {
-                    const collection = document.querySelectorAll('.clearfix.typing-animation.admin_chat');
-
-                    for (const elem of collection) {
-                        elem.remove();
-                    }
-
-                    let responseMessageHtml = `<li class="clearfix admin_chat">
-                                                    <span class="chat-img">
-                                                        <img src="/static/vendors/images/logo-icon.png" alt="">
-                                                    </span>
-                                                    <div class="chat-body clearfix">
-                                                        <p>`+response[assistantName]+`</p>
-                                                        <div class="chat_time">09:40PM</div>
-                                                    </div>
-                                                </li>`
-                    $(".chat-box ul li:last").before(responseMessageHtml);
-                    $("#mCSB_2").animate({ scrollTop: $("#mCSB_2")[0].scrollHeight}, {duration: 1000, complete: function(){
-                        // const chatContainerHeight = $('#mCSB_2_container').height()
-                        // $("#mCSB_2_container").css("top", -chatContainerHeight);
-                    }});       
-                },
-                error: function (data) {
-
+                    let employeeTableRow = `<tr>
+                                                <th scope="row">`+counter+`</th>
+                                                <!-- First Name -->
+                                                <td class="employeeFirstName">`+dataDic.first_name+`</td>
+                                                <!-- Last Name --> 
+                                                <td class="employeeLastName">`+dataDic.last_name+`</td>
+                                                <td><button type="button" class="btn btn-primary btn-sm bookMeetingBtn" data-toggle="modal" data-target="#bookMeetingModal" data-employee-id=`+dataDic.employee_id+`>Book a Meeting</button></td>
+                                                <td>
+                                                    <input class="bookedTimeSlotTags" style="width:400px;" type="text" name="bookedTimeSlotTags"/>
+                                                </td>
+                                            </tr>`
+                    
+                    $(".employeeTableByDate tbody tr:last").before(employeeTableRow);
+                    
+                    counter++   
                 }
-            });
+                bookMeetingBtns = $('.bookMeetingBtn');
+                for (let clickEventCounter = 0; clickEventCounter < bookMeetingBtns.length; clickEventCounter++) {
+                    bookMeetingBtns[clickEventCounter].addEventListener('click',  bookMeetingBtnsClickEvent.bind(this, clickEventCounter), false);
+                }
+                console.log(bookMeetingBtns)
+            },
+            error: function (data) {
+
+            }
+        });
+    }
+
+    function bookMeetingBtnsClickEvent(elementCounter, element){
+        clickedButton = bookMeetingBtns[elementCounter]
+        console.log("employee_id => ", clickedButton.getAttribute("data-employee-id"))
+        currentEmployeeId = clickedButton.getAttribute("data-employee-id")
+        $("#selectEmployeeDropdown").empty();
+        for (const dataDic of employeesDic) {
+            if(currentEmployeeId != dataDic.employee_id ){
+
+                let employeeDropdownOption = `<option value="{{ data.employee_id }}">`+dataDic.first_name+ " " + dataDic.last_name+`</option>`
+                
+                document.getElementById("selectEmployeeDropdown").innerHTML += employeeDropdownOption
+            
+            }
         }
 
-    // }, 2000);
+    }
 });
--- employees/views.py
+++ employees/views.py
@@ -4,10 +4,10 @@ import requests
 def home(request):
     # Render the HTML template home.html
     # response = requests.post('http://127.0.0.1:8000/api/employees/', json={"one": 1})
-    response = requests.get('http://127.0.0.1:8000/api/employees/')
-    print(response.json()) # This should contain the returned data
-    context = response.json()
-    return render(request, 'home.html', context)
+    # response = requests.get('http://127.0.0.1:8000/api/employees/2021-06-14/')
+    # print(response.json()) # This should contain the returned data
+    # context = response.json()
+    return render(request, 'home.html')
 
 
 # YYY-MM-DD
